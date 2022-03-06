bcaac85f210ef14ad5635820a6ec699e96e4b215
Reinier Kip <dev@rjkip.nl>
2022-03-06 15:04:04 +1300

    Display text instead of charts when SSR or no JS

--- src/lib/Charts.svelte
+++ src/lib/Charts.svelte
@@ -3,54 +3,59 @@
   import Line from '$lib/charts/Line.svelte';
   import ProcessingRate from '$lib/charts/ProcessingRate.svelte';
   import ProcessedPercentage from '$lib/charts/ProcessedPercentage.svelte';
+  import { browser } from '$app/env';
 </script>
 
-<div class="charts">
-  <div style="grid-area: processing-rate">
-    <ProcessingRate />
+{#if !browser}
+  <p>Charts are only available when JavaScript is enabled.</p>
+{:else}
+  <div class="charts">
+    <div style="grid-area: processing-rate">
+      <ProcessingRate />
+    </div>
+    <div style="grid-area: processed-percentage">
+      <ProcessedPercentage />
+    </div>
+    <div style="grid-area: cumulative-applications">
+      <CumulativeLine
+        title="Cumulative applications (total applications received)"
+        data={{
+          Applications: 'receivedApplications',
+          Approved: 'approvedApplications',
+          Declined: 'declinedApplications',
+        }}
+      />
+    </div>
+    <div style="grid-area: cumulative-people">
+      <CumulativeLine
+        title="Cumulative people (total people included in applications)"
+        data={{
+          People: 'receivedPeople',
+          Approved: 'approvedPeople',
+        }}
+      />
+    </div>
+    <div style="grid-area: applications">
+      <Line
+        title="Applications"
+        data={{
+          Applications: 'receivedApplications',
+          Approved: 'approvedApplications',
+          Declined: 'declinedApplications',
+        }}
+      />
+    </div>
+    <div style="grid-area: people">
+      <Line
+        title="People included in applications"
+        data={{
+          People: 'receivedPeople',
+          Approved: 'approvedPeople',
+        }}
+      />
+    </div>
   </div>
-  <div style="grid-area: processed-percentage">
-    <ProcessedPercentage />
-  </div>
-  <div style="grid-area: cumulative-applications">
-    <CumulativeLine
-      title="Cumulative applications (total applications received)"
-      data={{
-        Applications: 'receivedApplications',
-        Approved: 'approvedApplications',
-        Declined: 'declinedApplications',
-      }}
-    />
-  </div>
-  <div style="grid-area: cumulative-people">
-    <CumulativeLine
-      title="Cumulative people (total people included in applications)"
-      data={{
-        People: 'receivedPeople',
-        Approved: 'approvedPeople',
-      }}
-    />
-  </div>
-  <div style="grid-area: applications">
-    <Line
-      title="Applications"
-      data={{
-        Applications: 'receivedApplications',
-        Approved: 'approvedApplications',
-        Declined: 'declinedApplications',
-      }}
-    />
-  </div>
-  <div style="grid-area: people">
-    <Line
-      title="People included in applications"
-      data={{
-        People: 'receivedPeople',
-        Approved: 'approvedPeople',
-      }}
-    />
-  </div>
-</div>
+{/if}
 
 <style>
   @media (min-width: 1200px) {
