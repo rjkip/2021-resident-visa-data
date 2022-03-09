d94d14d2495dc05945a582e11ee533a30f01010b
Reinier Kip <dev@rjkip.nl>
2022-03-10 04:25:38 +1300

    Add processing rate 14-day moving average

--- src/lib/charts/ProcessingRate.svelte
+++ src/lib/charts/ProcessingRate.svelte
@@ -2,12 +2,29 @@
   import { processing } from '$lib/data/processing';
   import Chart from 'svelte-frappe-charts';
 
+  const processingRate = processing.map(
+    (row) => row.approvedApplications + row.declinedApplications,
+  );
+  const movingAverageDays = 14;
+  const movingAverage = processingRate.map((rate, index, array) => [
+    (
+      Array(movingAverageDays)
+        .fill(0)
+        .map((_, offset) => array[index - offset])
+        .reduce((acc, n) => acc + n, 0) / movingAverageDays
+    ).toFixed(),
+  ]);
+
   const chartData = {
     labels: processing.map((row) => row.date),
     datasets: [
       {
         name: 'Processing rate',
-        values: processing.map((row) => row.approvedApplications + row.declinedApplications),
+        values: processingRate,
+      },
+      {
+        name: `${movingAverageDays}-day moving average`,
+        values: movingAverage,
       },
     ],
   };
