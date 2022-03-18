01c01b27e737e44ce3e50cc02cab28d00b067b22
Reinier Kip <dev@rjkip.nl>
2022-03-19 09:59:22 +1300

    Include processing rate in "going-by-the-last-14-days" estimate

--- src/lib/charts/RemainingProcessedBy.svelte
+++ src/lib/charts/RemainingProcessedBy.svelte
@@ -1,11 +1,16 @@
 <script>
   import { processing } from '$lib/data/processing';
   import { formatLongDate, formatNum } from '../formatting';
+
+  const lastDay = processing[processing.length - 1];
 </script>
 
 <p class="notice">
   ✅ Going by the last 14 days, most of the remaining
-  <strong>{formatNum(processing[processing.length - 1].remainingApplications)}</strong>
+  <strong>{formatNum(lastDay.remainingApplications)}</strong>
   applications will be processed by
-  <strong>{formatLongDate(processing[processing.length - 1].processedBy)}</strong>.
+  <strong>{formatLongDate(lastDay.processedBy)}</strong>
+  at a rate of
+  <strong>{formatNum(lastDay.processedApplicationsMovAvg14)}</strong>
+  applications per day.
 </p>
