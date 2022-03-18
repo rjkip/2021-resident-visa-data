2da4fc276ff64f77f62f42143620ce2228bdb301
Reinier Kip <dev@rjkip.nl>
2022-03-19 10:15:19 +1300

    Number trend arrows

--- src/lib/charts/RemainingProcessedBy.svelte
+++ src/lib/charts/RemainingProcessedBy.svelte
@@ -1,16 +1,32 @@
 <script>
   import { processing } from '$lib/data/processing';
   import { formatLongDate, formatNum } from '../formatting';
+  import Trend from './Trend.svelte';
 
   const lastDay = processing[processing.length - 1];
+  const fortnightAgo = processing[processing.length - 1 - 14];
+  const processedByUp = lastDay.processedBy > fortnightAgo.processedBy;
+  const processedByDown = lastDay.processedBy < fortnightAgo.processedBy;
+  const processedApplicationsUp =
+    lastDay.processedApplicationsMovAvg14 > fortnightAgo.processedApplicationsMovAvg14;
+  const processedApplicationsDown =
+    lastDay.processedApplicationsMovAvg14 < fortnightAgo.processedApplicationsMovAvg14;
 </script>
 
 <p class="notice">
   ✅ Going by the last 14 days, most of the remaining
   <strong>{formatNum(lastDay.remainingApplications)}</strong>
   applications will be processed by
-  <strong>{formatLongDate(lastDay.processedBy)}</strong>
+  <strong>{formatLongDate(lastDay.processedBy)}</strong>&nbsp;<Trend
+    up={processedByUp}
+    down={processedByDown}
+    good="down"
+  />
   at a rate of
-  <strong>{formatNum(lastDay.processedApplicationsMovAvg14)}</strong>
+  <strong>{formatNum(lastDay.processedApplicationsMovAvg14)}</strong>&nbsp;<Trend
+    up={processedApplicationsUp}
+    down={processedApplicationsDown}
+    good="up"
+  />
   applications per day.
 </p>
---
+++ src/lib/charts/Trend.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let up = false;
+  export let down = false;
+  export let good = 'up';
+
+  $: symbol = up ? '▲' : down ? '▼' : null;
+</script>
+
+{#if symbol}
+  <span
+    class="trend"
+    class:up
+    class:down
+    class:good={(up && good === 'up') || (down && good === 'down')}
+    class:bad={(up && good === 'down') || (down && good === 'up')}>{symbol}</span
+  >
+{/if}
+
+<style>
+  .trend {
+    font-size: 0.8em;
+  }
+  .up {
+    vertical-align: top;
+  }
+  .down {
+    vertical-align: bottom;
+  }
+  .good {
+    color: green;
+  }
+  .bad {
+    color: #c70000;
+  }
+</style>
